361607231577c9453429eee0cd43fc228b3a98a0
Marx Alonso <marxchip99@gmail.com>
2025-05-28 23:37:32 -0500

    add panelmedico and design todo gerente

--- src/main/java/com/example/clinicalolimsa/repositories/AppUserRepository.java
+++ src/main/java/com/example/clinicalolimsa/repositories/AppUserRepository.java
@@ -3,8 +3,12 @@ package com.example.clinicalolimsa.repositories;
 import com.example.clinicalolimsa.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppUserRepository extends JpaRepository<AppUser,Integer> {
 
     public AppUser findByEmail(String email);
+    List<AppUser> findByRole(String role);
+
 }
 
