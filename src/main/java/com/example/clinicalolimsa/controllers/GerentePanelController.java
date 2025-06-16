2c4b4a43a549575cac7db01b52f2c39f742fa085
Marx Alonso <marxchip99@gmail.com>
2025-06-16 17:33:07 -0500

    agregando barras estadisticas

--- src/main/java/com/example/clinicalolimsa/controllers/GerentePanelController.java
+++ src/main/java/com/example/clinicalolimsa/controllers/GerentePanelController.java
@@ -1,12 +1,12 @@
 package com.example.clinicalolimsa.controllers;
 
-import com.example.clinicalolimsa.repositories.AppUserRepository;
+import com.example.clinicalolimsa.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Map;
+import java.util.List;
 
 @Controller
 public class GerentePanelController {
@@ -14,8 +14,24 @@ public class GerentePanelController {
     @Autowired
     private AppUserRepository userRepository;
 
+    @Autowired
+    private MedicosRepository medicoRepository;
+
+    @Autowired
+    private MedicamentosRepository medicamentosRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private ProveedorRepository proveedorRepository;
+
     @GetMapping("/gerente/gerentepanel")
     public String panelGerente(Model model) {
+        model.addAttribute("totalMedicos", medicoRepository.count());
+        model.addAttribute("totalMedicamentos", medicamentosRepository.count());
+        model.addAttribute("totalPacientes", pacienteRepository.count());
+
         return "/gerente/gerentepanel";
     }
 
--- src/main/java/com/example/clinicalolimsa/repositories/MedicamentosRepository.java
+++ src/main/java/com/example/clinicalolimsa/repositories/MedicamentosRepository.java
@@ -4,4 +4,5 @@ import com.example.clinicalolimsa.models.Medicamentos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicamentosRepository  extends JpaRepository<Medicamentos, Integer> {
+
 }
