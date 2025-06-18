9a15ba820d8076c608ff20437aca988a835b774c
JoMontoro <jmg050299@gmail.com>
2025-06-18 13:53:04 -0500

    nueva modificaciones

--- src/main/java/com/example/clinicalolimsa/controllers/HomeController.java
+++ src/main/java/com/example/clinicalolimsa/controllers/HomeController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping({"","/"})
-    public String home(){
+    @GetMapping({"", "/"})
+    public String home() {
         return "index";
     }
+
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
+
     @GetMapping("/contact")
-    public String contact(){
+    public String contact() {
         return "contact";
     }
 }
